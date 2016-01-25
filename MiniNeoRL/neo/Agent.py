7a3eafd2f75129740ca6f2af333c4a301b2b1d4a
Eric Laukien <ericlaukien@gmail.com>
2016-01-25 00:46:48 -0500

    Got rid of ER

--- MiniNeoRL/neo/Agent.py
+++ MiniNeoRL/neo/Agent.py
@@ -24,10 +24,11 @@ class Agent:
 
         self._exploration = np.zeros((numActions, 1))
 
-        self._replayBuffer = []
+        self._qPredictiveWeights = np.random.rand(1, layerSizes[0])
+        self._qPredictiveTraces = np.zeros((1, layerSizes[0]))
 
-        self._maxReplayBufferSize = 600
-        self._replayIterations = 40
+        self._qFeedBackWeights = np.random.rand(1, layerSizes[0])
+        self._qFeedBackTraces = np.zeros((1, layerSizes[0]))
 
         self._prevValue = 0.0
 
@@ -37,9 +38,9 @@ class Agent:
 
             if l == 0:
                 if l < len(layerSizes) - 1:
-                    layer = Layer(numInputs + numActions + 1, layerSizes[l], layerSizes[l + 1], initMinWeight, initMaxWeight, activeRatio)
+                    layer = Layer(numInputs + numActions, layerSizes[l], layerSizes[l + 1], initMinWeight, initMaxWeight, activeRatio)
                 else:
-                    layer = Layer(numInputs + numActions + 1, layerSizes[l], 1, initMinWeight, initMaxWeight, activeRatio)
+                    layer = Layer(numInputs + numActions, layerSizes[l], 1, initMinWeight, initMaxWeight, activeRatio)
             else:
                 if l < len(layerSizes) - 1:
                     layer = Layer(layerSizes[l - 1], layerSizes[l], layerSizes[l + 1], initMinWeight, initMaxWeight, activeRatio)
@@ -58,9 +59,6 @@ class Agent:
 
         for i in range(0, self._numActions):
             usedInputArr.append(self._actionsExploratory.item(i))
-
-        # Placeholder for q
-        usedInputArr.append(0.0)
 
         usedInput = np.matrix([ usedInputArr ]).T
 
@@ -81,62 +79,48 @@ class Agent:
                 self._layers[rl].downPass(np.matrix([[ 0 ]]), rl != 0)
 
         # Get PVi, LVe, LVi
-        q = self.getPrediction().item(self._numInputs + self._numActions)
+        q = 0.0
 
-        tdError = reward + qGamma * q - self._prevValue
-
-        newQ = self._prevValue + qAlpha * tdError
-
-        # Update older samples
-        g = qGamma
-
-        for i in range(0, len(self._replayBuffer)):
-            ri = len(self._replayBuffer) - 1 - i
-
-            self._replayBuffer[ri]._q += qAlpha * g * tdError
-
-            g *= qGamma
-
-        sample = None
-        
         if len(self._layers) > 1:
-            sample = ReplaySample(self._layers[0]._statesPrev, self._layers[1]._predictionsPrev, self._actions, self._actionsExploratory, self._prevValue, newQ)
+            q = np.dot(self._qPredictiveWeights, self._layers[0]._states) + np.dot(self._qFeedBackWeights, self._layers[1]._predictions)
         else:
-            sample = ReplaySample(self._layers[0]._statesPrev, np.matrix([[ 0 ]]), self._actions, self._actionsExploratory, self._prevValue, newQ)
+            q = np.dot(self._qPredictiveWeights, self._layers[0]._states)
 
-        self._replayBuffer.append(sample)
+        tdError = reward + qGamma * q.item(0) - self._prevValue
 
-        while len(self._replayBuffer) > self._maxReplayBufferSize:
-            self._replayBuffer.pop(0)
+        self._qPredictiveWeights += qAlpha * tdError * self._qPredictiveTraces
+
+        self._qPredictiveTraces = self._qPredictiveTraces * traceDecay + self._layers[0]._states.T
+
+        if len(self._layers) > 1:
+            self._qFeedBackWeights += qAlpha * tdError * self._qFeedBackTraces
+
+            self._qFeedBackTraces = self._qFeedBackTraces * traceDecay + self._layers[1]._predictions.T
 
         predInputArr = []
 
         for i in range(0, self._numInputs):
             predInputArr.append(input.item(i))
 
-        if tdError > 0.0:
-            for i in range(0, self._numActions):
+        for i in range(0, self._numActions):
                 predInputArr.append(self._actionsExploratory.item(i))
-        else:
-            for i in range(0, self._numActions):
-                predInputArr.append(self._actions.item(i))
-
-        predInputArr.append(newQ)
 
         predInput = np.matrix([ predInputArr ]).T
+
+        reinforce = tdError > 0.0
 
         # Learn
         for l in range(0, len(self._layers)):
             if l == 0:
                 if l < len(self._layers) - 1:
-                    self._layers[l].learn(predInput, self._layers[l + 1]._predictionsPrev, learnEncoderRate, learnDecoderRate, learnBiasRate, traceDecay)
+                    self._layers[l].learnRL(reinforce, predInput, self._layers[l + 1]._predictionsPrev, learnEncoderRate, learnDecoderRate, learnBiasRate, traceDecay)
                 else:
-                    self._layers[l].learn(predInput, np.matrix([[ 0 ]]), learnEncoderRate, learnDecoderRate, learnBiasRate, traceDecay)
+                    self._layers[l].learnRL(reinforce, predInput, np.matrix([[ 0 ]]), learnEncoderRate, learnDecoderRate, learnBiasRate, traceDecay)
             else:
                 if l < len(self._layers) - 1:
-                    self._layers[l].learn(self._layers[l - 1]._states, self._layers[l + 1]._predictionsPrev, learnEncoderRate, learnDecoderRate, learnBiasRate, traceDecay)
+                    self._layers[l].learnRL(reinforce, self._layers[l - 1]._states, self._layers[l + 1]._predictionsPrev, learnEncoderRate, learnDecoderRate, learnBiasRate, traceDecay)
                 else:
-                    self._layers[l].learn(self._layers[l - 1]._states, np.matrix([[ 0 ]]), learnEncoderRate, learnDecoderRate, learnBiasRate, traceDecay)
+                    self._layers[l].learnRL(reinforce, self._layers[l - 1]._states, np.matrix([[ 0 ]]), learnEncoderRate, learnDecoderRate, learnBiasRate, traceDecay)
 
         # Determine action
         self._exploration = (1.0 - explorationDecay) * self._exploration + explorationDecay * np.random.normal() * exploration
@@ -144,42 +128,9 @@ class Agent:
         for i in range(0, self._numActions):
             self._actions[i] = np.minimum(1.0, np.maximum(-1.0, self.getPrediction().item(self._numInputs + i)))
 
-            #self._actionsExploratory[i] = np.minimum(1.0, np.maximum(-1.0, self._actions[i] + self._exploration))
+            self._actionsExploratory[i] = np.minimum(1.0, np.maximum(-1.0, self._actions[i] + self._exploration))
 
-            if np.random.rand() < exploration:
-                self._actionsExploratory[i] = np.random.rand() * 2.0 - 1.0
-            else:
-                self._actionsExploratory[i] = self._actions[i]
-
-        # Experience replay
-        for iter in range(0, self._replayIterations):
-            replayIndex = np.random.randint(0, len(self._replayBuffer))
-
-            replaySample = self._replayBuffer[replayIndex]
-
-            # Activate off of old state
-            activations = np.dot(self._layers[0]._predictiveWeights, replaySample._states) + np.dot(self._layers[0]._feedBackWeights, replaySample._feedBack)
-
-            errorsArr = []
-
-            for i in range(0, self._numInputs):
-                errorsArr.append(0.0)
-
-            if replaySample._q >= replaySample._originalQ:
-                for i in range(0, self._numActions):
-                    errorsArr.append(replaySample._exploratoryAction.item(i) - activations.item(self._numInputs + i))
-            else:
-                for i in range(0, self._numActions):
-                    errorsArr.append(replaySample._action.item(i) - activations.item(self._numInputs + i))
-
-            errorsArr.append(replaySample._q - activations.item(self._numInputs + self._numActions))
-
-            errors = np.matrix([errorsArr]).T
-
-            self._layers[0]._predictiveWeights += replayAlpha * np.dot(errors, replaySample._states.T)
-            self._layers[0]._feedBackWeights += replayAlpha * np.dot(errors, replaySample._feedBack.T)
-
-        self._prevValue = q
+        self._prevValue = q.item(0)
 
     def getPrediction(self):
         return self._layers[0]._predictions
